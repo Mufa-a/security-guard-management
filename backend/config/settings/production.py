167d0a880c8a4326090fd9e796fd18d0592e93c4
Mufa-a <analystmufa@gmail.com>
2026-08-28 11:27:39 +0300

    fix: remove DATABASE_URL override in production settings

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -5,11 +5,7 @@ import os
 
 DEBUG = False
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL")
-    )
-}
+
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
